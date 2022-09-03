b966b749c224a1bfbf24293290f6bfc578af9e1d
Danya <kogdana@gmail.com>
2022-09-03 15:18:16 +0300

    Зробив бургер меню

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const factory = document.getElementById('burger');
+if(factory){
+    const isHeder = document.getElementById('heder');
+    factory.addEventListener("click", function(e){
+        isHeder.classList.toggle('_active');
+    }); 
+}
+
+const remout = document.getElementById('burger');
+if(factory){
+    const isHeder = document.getElementById('burger');
+    factory.addEventListener("click", function(e){
+        isHeder.classList.toggle('_activee');
+    }); 
+}
+
+const faactory = document.getElementById('fot');
+if(faactory){
+    const isfot = document.getElementById('fot');
+    faactory.addEventListener("click", function(e){
+        isfot.classList.toggle('_activ')
+    });
+}
