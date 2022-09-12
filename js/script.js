8e0d13bb8eac5264d81d61d64ea0a7c880c6d3cd
Danya <kogdana@gmail.com>
2022-09-12 18:07:13 +0300

    адапт для мобілки

--- js/script.js
+++ js/script.js
@@ -13,3 +13,11 @@ if(factory){
         isHeder.classList.toggle('_activee');
     }); 
 }
+
+const body = document.getElementById('boody');
+if(factory){
+    const isHeder = document.getElementById('boody');
+    factory.addEventListener("click", function(e){
+        isHeder.classList.toggle('_activa');
+    }); 
+}
